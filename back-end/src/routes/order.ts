388503b0934449c958bc100868b5b75919e9811b
mohamed salah <m48162698@gmial.com>
2025-05-17 23:06:00 +0300

    add api for orders

--- back-end/src/routes/order.ts
+++ back-end/src/routes/order.ts
@@ -9,6 +9,8 @@ const orderControler = new OrderControler(orderService);
 
 router.post("/order",(req,res)=>orderControler.createOrder(req,res))
 router.get("/order/:userId",(req,res)=>orderControler.getOrdersForUser(req,res))
+router.get("/order/admin/:adminId",(req,res)=>orderControler.getOrderForAdmin(req,res))
+router.get("/ordercomplate/:orderId",(req,res)=>orderControler.complateOreder(req,res))
 router.delete("/orders/delete/:userId",(req,res)=>orderControler.deleteAllOrders(req,res))
 router.delete("/order/delete/:orderId",(req,res)=>orderControler.deleteSpecificOrder(req,res))
 
